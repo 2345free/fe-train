eb95358f5e10dcb44c3d9d56967e14cab9a80cf8
luoxiaoxiao <2345free@sina.com>
2018-07-22 12:23:18 +0800

    spring 5以后不建议使用WebMvcConfigurerAdaptor,因为WebMvcConfigurer接口中有了默认实现(jdk8特性)

--- src/main/java/com/xiao/fe/train/MvcConfig.java
+++ src/main/java/com/xiao/fe/train/MvcConfig.java
@@ -2,14 +2,14 @@ package com.xiao.fe.train;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author: luoxiaoxiao
  * @date: 2018-07-21 16:11
  */
 @Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter {
+public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/sometry/index.html");
