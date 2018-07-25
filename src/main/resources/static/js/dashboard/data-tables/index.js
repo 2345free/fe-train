ad69174bb6828c623aa2797b11afdb1f0068b6b7
luoxiaoxiao <2345free@sina.com>
2018-07-25 12:27:33 +0800

    dataTables增加选择pageLength按钮,增加pageLength,select按钮国际化信息

--- src/main/resources/static/js/dashboard/data-tables/index.js
+++ src/main/resources/static/js/dashboard/data-tables/index.js
@@ -1,43 +1,58 @@
 $(function () {
 
     $('#table').DataTable({
-        "processing": true, // 必须加上这个才能显示加载中的效果
-        "serverSide": false,
-        "stateSave": true,
-        "paging": true, // 是否开启本地分页
-        "pagingType": "full_numbers",
-        "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]], // 定义每页显示数据数量
-        "displayLength": 5,
-        "ordering": true,
-        "info": true,
-        "autoWidth": true,
-        "language": {
+        processing: true, // 必须加上这个才能显示加载中的效果
+        serverSide: false,
+        stateSave: true,
+        paging: true, // 是否开启本地分页
+        pagingType: "full_numbers",
+        lengthMenu: [[5, 10, 25, 50, -1], /*后面的数组是显示翻译前面的数字*/[5, 10, 25, 50, "All"]], // 定义每页显示数据数量
+        displayLength: 5,
+        ordering: true,
+        info: true,
+        autoWidth: true,
+        language: {
             // 这里很重要，如果你的"加载中"是文字，则直接写上文字即可，如果是gif的图片，使用img标签就可以加载
-            // "processing": "<img src='/images/loading.gif'>",
-            "processing": '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>',
-            "lengthMenu": "显示 _MENU_ 项结果",
-            "zeroRecords": "没有匹配结果",
-            "info": "显示第 _START_ 至 _END_ 项结果,共 _TOTAL_ 项",
-            "infoEmpty": "显示第 0 至 0 项结果，共 0 项",
-            "infoFiltered": "(由 _MAX_ 项结果过滤)",
-            "search": "搜索:",
-            "emptyTable": "表中数据为空",
-            "loadingRecords": "载入中...",
-            "paginate": {
-                "first": "首页",
-                "previous": "上一页",
-                "next": "下一页",
-                "last": "末页"
+            // "processing: "<img src='/images/loading.gif'>",
+            processing: '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>',
+            lengthMenu: "显示 _MENU_ 项结果",
+            zeroRecords: "没有匹配结果",
+            info: "显示第 _START_ 至 _END_ 项结果,共 _TOTAL_ 项",
+            infoEmpty: "显示第 0 至 0 项结果，共 0 项",
+            infoFiltered: "(由 _MAX_ 项结果过滤)",
+            search: "搜索:",
+            emptyTable: "表中数据为空",
+            loadingRecords: "载入中...",
+            paginate: {
+                first: "首页",
+                previous: "上一页",
+                next: "下一页",
+                last: "末页"
             },
-            "aria": {
-                "sortAscending": ": 以升序排列此列",
-                "sortDescending": ": 以降序排列此列"
+            aria: {
+                sortAscending: ":以升序排列此列",
+                sortDescending: ":以降序排列此列"
+            },
+            buttons: {
+                pageLength: {
+                    '-1': '全部',
+                    _: '显示 %d 项结果'
+                }
+            },
+            select: {
+                rows: {
+                    _: '%d 行选中',
+                    0: '单击行选中',
+                    1: '1 行选中'
+                }
             }
-
         },
         // 文件导出
         dom: 'Bfrtip',
         buttons: [
+            {
+                extend: 'pageLength'
+            },
             {
                 extend: 'copyHtml5',
                 text: '<i class="fa fa-files-o"></i>',
@@ -96,19 +111,19 @@ $(function () {
             }
         ],
         select: true,
-        "ajax": {
-            "url": "/data.json",
-            // "contentType": "application/json",
-            "type": "GET",
-            "dataSrc": "data",
-            "data": function (param) {
+        ajax: {
+            url: "/data.json",
+            // "contentType: "application/json",
+            type: "GET",
+            dataSrc: "data",
+            data: function (param) {
                 param.timestamp = new Date().getTime();
                 // return JSON.stringify(param); // post请求
             }
         },
         //使用对象数组，一定要配置columns，告诉 DataTables 每列对应的属性
         //data 这里是固定不变的，name，position，salary，office 为你数据里对应的属性
-        "columns": [
+        columns: [
             {
                 data: 'name',
                 title: '姓名',
